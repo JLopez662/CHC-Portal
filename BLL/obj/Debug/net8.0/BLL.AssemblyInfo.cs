9b1800a46e58f9bc1a5970bf0387888c073bd43b
Jorge Lopez <jlopez@iri-pr.com>
2024-06-05 14:56:03 -0400

    Fix email sending issues

--- BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ed6fc6585266c8e801e63ed8554715c426ca6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27fdfb58fba176ac8adda417f157f44e40f31878")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CPA/obj/Debug/net8.0/CPA.AssemblyInfo.cs
+++ CPA/obj/Debug/net8.0/CPA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ed6fc6585266c8e801e63ed8554715c426ca6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27fdfb58fba176ac8adda417f157f44e40f31878")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
