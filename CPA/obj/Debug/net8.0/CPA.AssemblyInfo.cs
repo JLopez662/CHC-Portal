b4f8146501c6f4562197deafc923b00bc4e5c0ff
Jorge Lopez <jlopez@iri-pr.com>
2024-07-12 11:02:26 -0400

    Empty starting page on dashboard

--- CPA/obj/Debug/net8.0/CPA.AssemblyInfo.cs
+++ CPA/obj/Debug/net8.0/CPA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b5b06bc17a9c4f4e90c9191c05538c13de4935")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1732473f5317a9a46d03c8ecaf90ca5356d9be7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
