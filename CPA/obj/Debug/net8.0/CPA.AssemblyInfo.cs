296dd3a403e9ec2c704c3eea7d720baa1d7dce6a
execu <execu@JORGE>
2024-08-03 17:12:42 -0700

    Hide tabs when not searching

--- CPA/obj/Debug/net8.0/CPA.AssemblyInfo.cs
+++ CPA/obj/Debug/net8.0/CPA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba3dbb80cc2d65c4bece7a6fc9e8167b4e13996")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b57d8fc2558bfe8ed56dabf5bcdcbf2153ab841")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
